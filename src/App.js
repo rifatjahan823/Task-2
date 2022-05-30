3a796c423102dd7fa53585a42e85ae1830b34dc4
rifatjahan823 <rifatjahan823@gmail.com>
2022-05-30 12:37:27 +0600

    add carusel

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './Components/Footer/Footer';
+import Home from './Components/Home/Home/Home';
+import Login from './Components/Login/Login';
+import Navebar from './Components/Navbar/Navebar';
+import Register from './Components/Register/Register';
 
 function App() {
   return (
     <div className="App">
-
+      <Navebar></Navebar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/service" element={''} />
+          <Route path="/about" element={''} />
+          <Route path="/blog" element={''} />
+          <Route path="/login" element={<Login></Login>} />
+          <Route path="/register" element={<Register></Register>} />
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
