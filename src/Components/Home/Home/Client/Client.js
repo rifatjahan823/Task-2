f1aaf431d5a1ad6e45873ca6e15d35201f91a6de
rifatjahan823 <rifatjahan823@gmail.com>
2022-05-30 14:35:10 +0600

    complete client section

--- src/Components/Home/Home/Client/Client.js
+++ src/Components/Home/Home/Client/Client.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './Client.css'
+import bg from '../../../../img/client-section/trusted-services_pic_2360x714.ashx'
 
 const Client = () => {
     return (
-        <div id="client" className='container'>
-            
+        <div className='client-area' style={{backgroundImage: `url(${bg})`, backgroundPosition:"center",backgroundSize:"cover",backgroundRepeat:"no-repeat",position:"relative",color:'white',zIndex:'5',padding:"80px 0",backgroundAttachment:"fixed",marginTop:"50px"}}>
+         <div id="client" className='container'>
+         <h2 className='product-title mb-5'>Become <span style={{color:'#FF6F00'}}>a Client</span></h2>
+         <p>We have a lots of client See Our Client List <br></br> Bangladeshi Indicidual || Bangladeshi Institution || Foregin Investors || NRBs || Daily Market Company <br></br> We always Provide Our Best Services</p>
+            <button className='btn'>See More</button>
+         </div>
         </div>
     );
 };
--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Product from '../Product/Product';
+import About from './About/About';
+import Client from './Client/Client';
 
 const Home = () => {
     return (
         <div>
            <Banner></Banner> 
            <Product></Product>
+           <Client></Client>
+           <About></About>
         </div>
     );
 };
