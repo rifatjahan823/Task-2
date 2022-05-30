975b7ccbdc063ccdddb9d321812e5740218f54bf
rifatjahan823 <rifatjahan823@gmail.com>
2022-05-30 14:43:01 +0600

    complete about section

--- src/Components/Home/Home/About/About.js
+++ src/Components/Home/Home/About/About.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleArrowRight} from '@fortawesome/free-solid-svg-icons';
+import about from '../../../../img/about/money-financial-business-growth-concept-1223989.jpg'
 
 const About = () => {
     return (
-        <div id="about" className='container'>
-            
+        <div id="about" className='container mt-5'>
+            <h2 className='product-title mb-5'>Product <span style={{color:'#FF6F00'}}>and Service</span></h2>
+            <div className='row g-4 align-items-center '>
+            <div className='col-md-6 col-12'>
+                    <img className='img-fluid' src={about} alt="" />
+                </div>
+                <div className='col-md-6 col-12 text-md-start text-center'>
+                    <div className='product-content'>
+                    <div className='row'>
+                    <div className='col-md-6 col-12'>
+                 <p>  <FontAwesomeIcon className='icon' icon={faCircleArrowRight} /> Capital Profile</p>
+                   <p> <FontAwesomeIcon className='icon' icon={faCircleArrowRight} /> Mission Version</p>
+                        <p><FontAwesomeIcon className='icon' icon={faCircleArrowRight} /> Board and Directors</p>
+                       <p> <FontAwesomeIcon className='icon' icon={faCircleArrowRight} /> Contracts </p>
+                    </div>
+                    <div className='col-md-6 col-12'>
+                      <p><FontAwesomeIcon className='icon' icon={faCircleArrowRight} /> Management Excutive</p>
+                    <p><FontAwesomeIcon className='icon' icon={faCircleArrowRight} /> IPO Circulation</p>
+                    <p><FontAwesomeIcon className='icon' icon={faCircleArrowRight} /> Career oppournity</p>
+                    </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
